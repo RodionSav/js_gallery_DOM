7c0fe60f2c5c4248e95f37de8c01b1a8ddd06bf9
Rodionsav <rodionsav@mate.academy>
2023-12-30 01:10:13 +0100

    make build

---
+++ index.b442fd40.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".gallery__large-img");document.querySelector(".gallery__list").addEventListener("click",function(t){if(t.preventDefault(),"IMG"===t.target.tagName){var r=t.target.closest(".list-item__link");e.src=r}});//# sourceMappingURL=index.b442fd40.js.map
+
+//# sourceMappingURL=index.b442fd40.js.map
